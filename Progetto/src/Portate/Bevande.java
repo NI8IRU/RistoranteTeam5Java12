a8b439951678f96d3407b2a015f1ede2a4f5313d
NI8IRU <francesco.tornambe@gmail.com>
2023-03-25 00:44:17 +0100

    Creata la classe Bevande: impostate le variabili locali, creati i constructor, i setter ed i getter

--- Progetto/src/Portate/Bevande.java
+++ Progetto/src/Portate/Bevande.java
@@ -1,4 +1,38 @@
 package Portate;
 
 public class Bevande {
+
+    private String nome = null;
+    private Double prezzo = null;
+    private Double percentAlcol = 0.0;
+
+    Bevande(String nome, Double prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+    Bevande(String nome, Double prezzo, Double percentAlcol) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.percentAlcol = percentAlcol;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void setPercentAlcol(Double percentAlcol) {
+        this.percentAlcol = percentAlcol;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public Double getPrezzo() {
+        return prezzo;
+    }
+    public Double getPercentAlcol() {
+        return percentAlcol;
+    }
 }
