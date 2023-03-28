bef84a81893c5bac69d1e3cf21ca34b5abae7075
Antonio Spinola <onibi93@gmail.com>
2023-03-28 14:15:35 +0200

    Aggiunta classe secondi piatti !

---
+++ Progetto/src/Portate/SecondiPiatti.java
@@ -0,0 +1,24 @@
+package Portate;
+
+public class SecondiPiatti {
+    private String name = null;
+    private Float price = null;
+
+    public SecondiPiatti(String nome, Float prezzo) {
+        this.name = nome;
+        this.price = prezzo;
+    }
+    public String getNome() {
+        return name;
+    }
+    public Float getPrezzo() {
+        return price;
+    }
+
+    public void setNome(String nome) {
+        this.name = nome;
+    }
+    public void setPrezzo(Float prezzo) {
+        this.price = prezzo;
+    }
+}
