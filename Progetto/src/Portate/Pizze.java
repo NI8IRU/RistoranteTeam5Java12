9c31943136b4f0c6815a1b2cf17c891c9e136e7e
Katacooper <francescoiliceto0@gmail.com>
2023-03-29 10:49:58 +0200

    Creata classe pizze

---
+++ Progetto/src/Portate/Pizze.java
@@ -0,0 +1,30 @@
+public class Pizze {
+    private String name;
+    private double price;
+
+    public Pizze(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void printPizza(){
+        System.out.print("Name of the pizza: " + name);
+        System.out.println(" || Price of the pizza:" + price);
+    }
+}
