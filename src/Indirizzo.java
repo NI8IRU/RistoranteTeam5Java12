9a600de6a49550597fe928d5acab9ba070ffd5ed
FilippoRocchi <filipporocchi00@gmail.com>
2023-04-27 15:25:45 +0200

    aggiunto record indirizzo

---
+++ src/Indirizzo.java
@@ -0,0 +1,9 @@
+package src;
+
+public record Indirizzo(String via, String provincia, int numCivico, int cap, String city, String infoAggiuntive) {
+    public void printIndirizzo(){
+        System.out.println(via + " " + numCivico + ", " + city + "\n" +
+                provincia + ", " +  cap + "\n" +
+                infoAggiuntive);
+    }
+}
