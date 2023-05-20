6ccf2d054c3d822a1600cd56c6400baba9b096af
Fab11 <fab.stazi11@gmail.com>
2023-05-20 15:32:13 +0200

    Riportato come prima la classe PrimoPiatto, aggiunti piccole descrizione al codice, la classe manca ancora di un parametro nel super ma questa mancanza verrà sistemata a breve.

--- src/main/java/portate/PrimoPiatto.java
+++ src/main/java/portate/PrimoPiatto.java
@@ -1,4 +1,44 @@
 package portate;
 
-public class PrimoPiatto {
+
+import enumertion.OrdinatoreEnum;
+
+
+public class PrimoPiatto extends Portata {
+    private Integer kcalPerEtto;
+
+
+    public PrimoPiatto(String nome, Double prezzo, Integer kcalPerEtto) {
+        //messo il valore dell'ordinatore nel super!
+        super(nome, prezzo, OrdinatoreEnum.PRIMA_POSIZIONE);
+        this.kcalPerEtto = kcalPerEtto;
+
+    }
+
+    /**
+     * @return kcalPerEtto
+     */
+
+    public Integer getKcalPerEtto() {
+        return kcalPerEtto;
+    }
+
+
+    /**
+     * @param kcalPerEtto
+     */
+    public void setKcalPerEtto(Integer kcalPerEtto) {
+        this.kcalPerEtto = kcalPerEtto;
+    }
+
+
+    /**
+     * Override della funzione ereditata
+     * dalla classe padre 'Portata'
+     */
+    @Override
+    public void printPortata() {
+        System.out.println("  " + super.getNome() + ": " + super.getPrezzo() + "€"
+                + " \n  " + getKcalPerEtto() + "kcal (presenti per etto); \n");
+    }
 }
